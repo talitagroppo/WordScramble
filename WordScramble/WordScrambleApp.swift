11eab5b95031a9daa5577de7c3efc379a90ad63c
Talita Groppo <talitagroppo@icloud.com>
2021-02-02 13:50:52 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Talita Groppo on 31/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
